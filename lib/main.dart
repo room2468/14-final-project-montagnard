50168a3a53357781b456a445d8aa47a5a3c55c8f
aulialvianto <pitot.crush@gmail.com>
2021-06-14 14:37:42 +0700

    Fixing Project

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Tambahkan Item',
+      title: 'Gudang Kopi',
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
